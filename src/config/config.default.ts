c7afa4be57139ecfb81bb91ead8d0aaee1d14bff
王一凡 <709862426@qq.com>
2025-06-24 09:02:07 +0800

    new

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1750726624605_3882',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
